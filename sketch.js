692cec23005ac56ecb77e64c6d1f154ebda38474
sankalpshibad1 <87346870+sankalpshibad1@users.noreply.github.com>
2021-08-04 21:03:01 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ var path, pathImg;
 
 function preload(){
   //pre-load images
-runnerImg = loadImage("Runner-1.png","Runner-2.png");
+runnerImg = loadAnimation("Runner-1.png","Runner-2.png");
 pathImg = loadImage("path.png");
 
 }
 
 function setup(){
-  createCanvas(400,1000);
+  createCanvas(400,400);
 
   //create sprites here
 
@@ -19,12 +19,12 @@ function setup(){
   path.scale = 1.2;
   
 
-  runner = createSprite(300,300,40,40);
-  runner.addImage(runnerImg);
+  runner = createSprite(180,340,30,30);
+  runner.addAnimation("Jakerunning",runnerImg);
   runner.scale = 0.1;
 
-  invisibleGround1 = createSprite(50,50,20,400);
-  invisibleGround2 = createSprite(350,350,20,400);
+  invisibleGround1 = createSprite(0,0,100,800);
+  invisibleGround2 = createSprite(410,0,100,800);
   invisibleGround1.visible = false;
   invisibleGround2.visible = false;
 
@@ -33,16 +33,20 @@ function setup(){
 function draw() {
   background(0);
 
+  path.velocityY = 4;
+
+  runner.x = World.mouseX;
+
+  edges= createEdgeSprites();
+  runner.collide(edges[3]);
+  runner.collide(invisibleGround1);
+  runner.collide(invisibleGround2);
+
   if(path.y > 400){
     path.y = height/2;
   }
 
- runner.collide(invisibleGround1,invisibleGround2);
-
-
-function mousePressed(){
-  runner.velocityX = 5;
-}
+ 
 
 drawSprites();
 
